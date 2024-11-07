17893d29eb06daa9ac9e574bd4e5698aae5f091e
Samantha <sraby4723@proton.me>
2024-11-06 18:10:27 -0600

    Moved index.html to app.vue for server to run

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+		<h1>Project Prototype</h1>
+		<h2>Search for a class via number or name:</h2>
+		<form action="">
+			<label for="dbsearch">Search the Database:</label>
+			<input type="search" id="dbsearch" name="dbsearch">
+			<input type="submit" value="Submit">
+		</form>
   </div>
 </template>
