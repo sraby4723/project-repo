bcf1ecb9c6642ae70d55199d1fe3f8db3155f374
Samantha <sraby4723@proton.me>
2024-11-07 14:31:21 -0600

    Added command to run server, plus id for homepage

--- app.vue
+++ app.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-		<h1>Project Prototype</h1>
+	<div id = "main_page">
+		<h1>Vue WIP</h1>
 		<h2>Search for a class via number or name:</h2>
 		<form action="">
 			<label for="dbsearch">Search the Database:</label>
 			<input type="search" id="dbsearch" name="dbsearch">
 			<input type="submit" value="Submit">
 		</form>
-  </div>
+	</div>
+	<script>
+	
+	</script>
 </template>
